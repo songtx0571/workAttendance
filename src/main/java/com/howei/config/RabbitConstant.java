e31ad891ad34e05d79d6fb5330bdfec2a8aa08c8
jianghao <299@qq.com>
2021-04-22 14:22:25 +0800

    修复报销统计Bug

--- src/main/java/com/howei/config/RabbitConstant.java
+++ src/main/java/com/howei/config/RabbitConstant.java
@@ -2,13 +2,13 @@ package com.howei.config;
 
 public class RabbitConstant {
     //交换机名称
-    public final static String EXCHANGE = "exchange_test";
+    public final static String EXCHANGE = "exchange_Test";
     //队列
-    public final static String QUEUE_TRANSACTION = "queue_transaction";
-    public final static String QUEUE_CONTRACT = "queue_contract";
-    public final static String QUEUE_QUALIFICATION = "queue_qualification";
+    public final static String QUEUE_TRANSACTION = "queue_transactionTest";
+    public final static String QUEUE_CONTRACT = "queue_contractTest";
+    public final static String QUEUE_QUALIFICATION = "queue_qualificationTest";
     //路由key
-    public final static String RK_TRANSACTION = "transaction";
-    public final static String RK_CONTRACT = "contract";
-    public final static String RK_QUALIFICATION = "qualification";
+    public final static String RK_TRANSACTION = "transactionTest";
+    public final static String RK_CONTRACT = "contractTest";
+    public final static String RK_QUALIFICATION = "qualificationTest";
 }
